194f022d91bb9f7493a498cc10a53e376bf13f66
bogdan.druziuk <prDkqUvddxLwSsc91LtS>
2021-03-06 16:56:15 +0200

    Command button for ribbon created

---
+++ src/Feature/PresentationVisualizer/code/Commands/Visualize.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Text;
+using Sitecore.Web.UI.Sheer;
+
+namespace Feature.PresentationVisualizer.Commands
+{
+    public class Visualize : Command
+    {
+        /// <summary>Executes the command in the specified context.</summary>
+        /// <param name="context">The context.</param>
+        public override void Execute(CommandContext context)
+        {
+            Assert.ArgumentNotNull((object)context, nameof(context));
+
+            if (context.Items.Length != 1)
+                return;
+
+            Item obj = context.Items[0];
+
+            UrlString urlString = new UrlString(UIUtil.GetUri("/sitecore modules/Web/PresentationVisualizer/Visualization.aspx"));
+            urlString.Append("id", obj.ID.ToString());
+            SheerResponse.ShowModalDialog(urlString.ToString());
+        }
+    }
+}
